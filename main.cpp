a8fb5c4f4b0482e4ecce7900f8109dda2a0e42fa
Zeus3252 <33201512+Zeus3252@users.noreply.github.com>
2020-05-15 17:19:34 -0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include "schedule.h"
+#include "CSVReader.h"
+#include <algorithm> 
+#include <stdlib.h>
+#include <string>
+#include <limits>
+#include <algorithm>
+
+/*
+<project firstname="Jayson" Lastname="Brown" projectname="Scheduler Program" />
+Purpose: This program displays a schedule when the user inputs 1. The user can search for entries by Day (inputs 2) or Activity (inputs 3).
+*/
+
+using namespace std;
+
+int main(int argc, char** argv) {
+	cout<<"***************************************"<<endl<<"Welcome to Jayson's Scheduler         |"<<endl<<"***************************************"; 
+	cout<<endl<<"                MENU                  |"<<endl<<"---------------------------------------"<<endl;
+    cout<<"Press [1] to see all schedules."<<endl<<"Press [2] to see schedules for a given day."<<endl<<"Press [3] to see schedules for a given activity."<<endl<<"Enter anything else to exit.";
+	int menuOption;
+	cin>>menuOption;
+	Time myObj;
+	if(menuOption == 1){
+		myObj.optionOne();
+	}
+	if(menuOption == 2){
+		myObj.optionTwo();
+	}
+	if(menuOption == 3){
+		myObj.optionThree();
+	}else{
+		exit(0);
+	}
+	return 0;
+}
