156572342b34cb4769989ba3af8a507db8dede5b
Zeus3252 <33201512+Zeus3252@users.noreply.github.com>
2021-04-03 02:48:37 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@
 #include <algorithm>
 
 /*
-<project firstname="Jayson" Lastname="Brown" projectname="Scheduler Program" />
+<projectname="Scheduler Program" />
 Purpose: This program displays a schedule when the user inputs 1. The user can search for entries by Day (inputs 2) or Activity (inputs 3).
 */
 
